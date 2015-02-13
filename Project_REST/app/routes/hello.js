03715b17f7a57eb1f92b0f858e4f8e188bcc5a73
Remi Mollard <remimollard38@gmail.com>
2015-02-13 14:46:44 +0100

    Remastering Full JS

--- Project_REST/app/routes/hello.js
+++
@@ -1,8 +0,0 @@
-
-module.exports = function(app) {
-    //--- CRUD --------------------------------	---------------------------
-    // get Hello
-    app.get('/hello',function(req, res) {
-        res.status(200).send('Hello NodeJS 2015')
-    });
-};
